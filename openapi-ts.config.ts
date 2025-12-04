110d97670c520b3c5d7112b6224977cf592b6af7
Linus Fischer <leicraft@leicraftmc.de>
2025-12-04 19:12:01 +0100

    update

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-    input: 'http://localhost:3003/docs/openapi',
+    input: 'http://localhost:12151/docs/openapi',
     output: 'app/api-client',
     plugins: [
         '@hey-api/client-nuxt'
